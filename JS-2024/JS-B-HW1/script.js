8f1ef8f32819fa3d0ddaefe489a13d57cab53b86
abab016001 <abab016001@gmail.com>
2024-10-06 23:45:35 +0800

    課前最終作業一提交

---
+++ JS-2024/JS-B-HW1/script.js
@@ -0,0 +1,5 @@
+let img_path = "https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023week1/";
+document.getElementById("vito").setAttribute("src", img_path + "vito.png");
+document.getElementById("fb").setAttribute("src", img_path + "fb.png");
+document.getElementById("instagram").setAttribute("src", img_path + "instagram.png");
+document.getElementById("line").setAttribute("src", img_path + "line.png");
