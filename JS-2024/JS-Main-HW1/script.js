b5ffc6c61fd601fa29bd1a9a21d0b8aef0fc2539
abab016001 <abab016001@gmail.com>
2024-10-12 00:56:20 +0800

    1131011

--- JS-2024/JS-Main-HW1/script.js
+++ JS-2024/JS-Main-HW1/script.js
@@ -16,13 +16,11 @@ let walletTotal = 200;
 let tmp_cost = 0;
 let cost = (ricePrice*2) + (steakPrice);
 let fishSteakCount = 0;
-while (1 == 1) {
-    tmp_cost = cost + (fishSteakPrice*fishSteakCount);
+while (tmp_cost < walletTotal) {
+    tmp_cost = cost + (fishSteakPrice * ++fishSteakCount);
     if (tmp_cost >= walletTotal) {
         fishSteakCount--;
         break;
-    } else {
-        fishSteakCount++;
     }
 }
 cost += (fishSteakPrice*fishSteakCount);
@@ -30,11 +28,12 @@ console.log(`小明買完飯了，他一共剩下${walletTotal-cost}元`)
 /***   作業 3 填答區結束   **/
 
 /***   作業 4 填答區開始   **/
-let currentLight = "red";
+let isWaitting = true;
+let trafficLight = "red";
 let people = 10;
-let secondes = 35;
-let cloud = 3;
-let sun = 1;
+let seconds = 35;
+let cloudCnt = 3;
+const sunCnt = 1;
 /***   作業 4 填答區結束   **/
 
 /***   作業 5 填答區開始   **/
@@ -54,7 +53,7 @@ let a = 1;//範例﹕宣告了一個a的變數，並賦予了一個1的數字型
 let b = 0;// 宣告了一個b的變數，並賦予了一個0的數字型別
 a=3;// 賦予變數a一個3的數字型別
 a=b+2;// 賦予變數a(變數b與一個3的數字型別之加總)
-a-b;// 什麼事都沒做
+a-b;// 變數b與變數a之加總
 b+=1;// 賦予變數b(變數b與一個1的數字型別之加總)
 // Ans: a = 5
 // Ans: b = 1
@@ -62,13 +61,6 @@ b+=1;// 賦予變數b(變數b與一個1的數字型別之加總)
 
 /***   作業 7 填答區開始   **/
 //此題目不用在 codepen 上作答，請同學用小畫家製作記憶體指向圖片，並藉由程式勇者村系統的任務留言介面上傳您的答案圖片即可
-let arr1 = [1,2,3];
-let arr2 = arr1;
-
-arr2.push(4);
-arr2 = [5,6,7];
-console.log("作業 7");
-console.log(arr1, arr2);
 
 /***   作業 7 填答區結束   **/
 
